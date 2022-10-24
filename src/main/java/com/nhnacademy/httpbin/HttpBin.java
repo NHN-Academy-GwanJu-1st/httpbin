ec7d8dd9bb6896b46b4b5b0e0676587d1e4492f9
MyeoungDev <73057935+MyeoungDev@users.noreply.github.com>
2022-10-24 23:59:31 +0900

    안되는 부분 TODO 작성

--- src/main/java/com/nhnacademy/httpbin/HttpBin.java
+++ src/main/java/com/nhnacademy/httpbin/HttpBin.java
@@ -4,10 +4,10 @@ import java.io.*;
 import java.net.*;
 import java.util.StringTokenizer;
 
-public class HttpBin{
+public class HttpBin implements Runnable{
 
-    private static final int PORT = 80;
-    private static final String HOST = "test-vm.org";
+    private static final int PORT = 12345;
+    private static final String HOST = "test-vm.com";
     private static final String USER_AGENT = "User-Agent: curl/7.68.0";
     private static final String ACCEPT = "Accept: */*";
 
@@ -16,32 +16,52 @@ public class HttpBin{
     private String method;
     private boolean verbos;
 
-    public HttpBin() {
-        this.verbos = false;
+    private Socket socket;
+    public HttpBin(Socket socket) {
+        this.socket = socket;
         this.method = "GET";
+        this.verbos = false;
     }
 
     public static void main(String[] args) throws IOException {
 
-        if (!args[0].equals("curl")) {
-            throw new RuntimeException("잘못된 명령어 입니다.");
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("Server Start");
+
+            while (true) {
+                HttpBin myServer = new HttpBin(serverSocket.accept());
+                System.out.println("Server Connect Success!");
+
+                Thread thread = new Thread(myServer);
+                thread.start();
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Server Connect Fail!");
         }
+    }
 
-        HttpBin httpBin = new HttpBin();
+    @Override
+    public void run() {
 
-        start(httpBin, args);
-
+        /* TODO ## 서버를 127.0.0.1 로 열어줘서
+        *   curl localhost는 정상 작동함.
+        *   하지만  test-vm을 할경우 다른 ip로 시도되어짐
+        *   hosts 파일 설정을 바꿔도 소용이 없음....*/
 
     }
 
-    public static void start(HttpBin httpBin, String[] args) throws IOException {
+    public void run(HttpBin httpBin, String[] args) throws IOException {
 
         httpBin.optionCheck(args);
         /* curl -v http://test-vm/ip */
 
         URL url = new URL(httpBin.url);
 
+
         Socket socket = new Socket();
+
         SocketAddress address = new InetSocketAddress(url.getHost(), PORT);
 
         System.out.println("url getHOst: " + url.getHost());
@@ -50,7 +70,6 @@ public class HttpBin{
         /* 접속 IP 주소 */
         String clientIp = InetAddress.getLocalHost().getHostAddress();
         System.out.println(clientIp);
-
         socket.connect(address);
 
         StringBuilder outBuilder = new StringBuilder();
